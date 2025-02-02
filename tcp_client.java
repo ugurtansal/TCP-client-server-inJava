7777e48e1476925b2c8778c5f198d945e765edff
ugurtansal <ugur.tansal@gmail.com>
2025-02-03 00:24:08 +0300

    added client and server codes

---
+++ tcp_client.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.net.*;
+
+public class TCPClient {
+    public static void main(String[] args) {
+        String sentence;
+        String modifiedSentence;
+        String serverIP = "YOUR_API_ADDRESS"; // Server IP address,
+        int serverPort = 5000; // Server port , it can be change
+
+        try (BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+             Socket clientSocket = new Socket(serverIP, serverPort);
+             DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+             BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+
+            System.out.print("Enter a sentence: ");
+            sentence = inFromUser.readLine();
+
+            outToServer.writeBytes(sentence + "\n");
+            modifiedSentence = inFromServer.readLine(); // We wait answer from server
+            System.out.println("FROM SERVER: " + modifiedSentence);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ tcp_server.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.net.*;
+
+public class TCPServer {
+    public static void main(String[] args) {
+        int port = 5000;//Port number can be changed , but must be same client port number
+
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            System.out.println("TCP Server is running on port " + port);
+
+            while (true) {
+                Socket connectionSocket = serverSocket.accept(); 
+                System.out.println("Client connected: " + connectionSocket.getInetAddress());
+
+                BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+                DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+
+                String clientSentence = inFromClient.readLine(); // İstemciden mesaj al
+                System.out.println("Received: " + clientSentence);
+
+                String modifiedSentence = clientSentence.toUpperCase() + "\n"; 
+                outToClient.writeBytes(modifiedSentence); //Returne client message but with uppercase
+
+                connectionSocket.close(); 
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
